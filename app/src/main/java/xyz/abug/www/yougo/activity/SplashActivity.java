40fbb7322b67ce61fb6f0dce4c794dfd0c404d50
gorrywang <isgorry@gmail.com>
2017-06-23 10:33:20 +0800

    修复大多数

--- app/src/main/java/xyz/abug/www/yougo/activity/SplashActivity.java
+++ app/src/main/java/xyz/abug/www/yougo/activity/SplashActivity.java
@@ -3,6 +3,8 @@ package xyz.abug.www.yougo.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
 
 import xyz.abug.www.yougo.R;
 
@@ -11,6 +13,11 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // 隐藏标题栏
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        // 隐藏状态栏
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
         new Thread() {
             @Override
